6a21300f8a407d84a93289e93e8cf2ff862e84b1
Ragib Shahriar <ragib.hridoy01@gmail.com>
2025-08-10 12:18:02 +0600

    [WIP] added chnages in django admin

--- src/custom_admin/admin.py
+++ src/custom_admin/admin.py
@@ -4,21 +4,27 @@ from django.contrib.auth.admin import UserAdmin
 
 class SalesAuthUserAdmin(UserAdmin):
     model = SalesAuthUser
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined')
-    list_filter = ('is_active', 'is_staff', 'date_joined')
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'date_joined')
     search_fields = ('email', 'username', 'first_name', 'last_name')
     ordering = ('-date_joined',)
     filter_horizontal = ()
+    
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('username', 'first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff')}),
-        ('Important dates', {'fields': ('date_joined',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('date_joined', 'last_login')}),
     )
+    
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'username', 'first_name', 'last_name', 'is_active', 'is_staff'),
+            'fields': (
+                'email', 'password1', 'password2', 
+                'username', 'first_name', 'last_name', 
+                'is_active', 'is_staff', 'is_superuser'
+            ),
         }),
     )
 
